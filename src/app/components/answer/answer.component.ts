f467ab96e3933a95fe67a3e11a7c4b7d741c12a3
jurgenru <jurgenruegnberg@gmail.com>
2022-01-19 17:50:28 -0400

    check click appear

--- src/app/components/answer/answer.component.ts
+++ src/app/components/answer/answer.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnswerComponent implements OnInit {
 
+  icon!: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  oneClick() {
+    console.log(this.icon);
+    if (this.icon === 'bi bi-check') {
+      this.icon = 'bi bi-check-all'
+      console.log('click ' + this.icon);
+    } else if (this.icon === undefined || this.icon === '') {
+      this.icon = 'bi bi-check'
+      console.log('click ' + this.icon);
+    } else {
+      this.icon = ''
+      console.log('click ' + this.icon);
+    }
+  }
 }
