90ef394b4a79b4fcbb4d6348c876e7d7ba9f9273
jurgenru <jurgenruegnberg@gmail.com>
2022-01-30 20:44:41 -0400

    show content on each input for formControlName

--- src/app/poll-creation/edit/edit.component.ts
+++ src/app/poll-creation/edit/edit.component.ts
@@ -13,7 +13,8 @@ import { PollSaveService } from 'src/app/services/poll-save.service';
 })
 export class EditComponent implements OnInit {
 
-  options: any[] = [];
+  options: option[] = [];
+  filteredOption!: any
 
   constructor(private activeRouter: ActivatedRoute,
     private router: Router, private pollService: PollSaveService,
@@ -37,14 +38,12 @@ export class EditComponent implements OnInit {
     console.log(pollId);
     this.pollService.getPollById(pollId).subscribe((data: any) => {
       this.pollData = data;
+      this.filteredOption = this.pollData.textOptions?.filter(o => o.content !== '');
       this.editPoll.setValue({
         'title': this.pollData.title,
         'description': this.pollData.description,
         'calendarOptions': this.pollData.calendarOptions,
-        'textOptions': this.pollData.textOptions,
-        /*?.map(option => {
-          return this.options.push(option.content.toString())
-        })*/
+        'textOptions': this.filteredOption,
         'needBe': this.pollData.needBe,
         'numberVote': this.pollData.numberVote,
         'singleVote': this.pollData.singleVote,
