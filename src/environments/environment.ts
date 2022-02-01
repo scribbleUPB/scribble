09c7c7417f42ce0a9cf2296a6e45c27e4eab844c
jurgenru <jurgenruegnberg@gmail.com>
2022-02-01 13:32:00 -0400

    ready for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7920/api/'
-  //apiUrl: 'http://skynet.lp.upb.edu:7920/api/'
+  //apiUrl: 'http://localhost:7920/api/'
+  apiUrl: 'http://skynet.lp.upb.edu:7920/api/'
 };
 
 /*
